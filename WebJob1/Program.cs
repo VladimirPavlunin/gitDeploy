62640049485000e9a5dae18cc7fee721c80e1358
Vladimir <v.pavlunin@paralect.com>
2017-01-03 16:08:17 +0300

    Adding webjob

---
+++ WebJob1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace WebJob1
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            Console.WriteLine("Starting....");
+            while (true)
+            {
+                Console.WriteLine("Working....");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
