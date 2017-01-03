9555dbd4872109a85d87604322df9eed87ad4ef2
Vladimir <v.pavlunin@paralect.com>
2017-01-03 16:15:15 +0300

    Adding scheduler

---
+++ Scheduler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Scheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey from scheduler");
+            while (true)
+            {
+                Console.WriteLine("I am working...");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
